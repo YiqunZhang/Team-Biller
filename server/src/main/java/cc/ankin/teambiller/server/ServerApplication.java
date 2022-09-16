e275c0b915683bc88ca3125e6eb62ecca9a1d783
yiqunzhang <u7102332@anu.edu.au>
2022-09-16 15:56:50 +1000

    init server springboot

---
+++ server/src/main/java/cc/ankin/teambiller/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package cc.ankin.teambiller.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/cc/ankin/teambiller/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.ankin.teambiller.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4c7e7333b61a49ff43290ff750c55901cecf9dbb
yiqunzhang <2223729465@qq.com>
2022-09-20 22:43:53 +1000

    some test

--- server/src/test/java/cc/ankin/teambiller/server/ServerApplicationTests.java
+++ server/src/test/java/cc/ankin/teambiller/server/ServerApplicationTests.java
@@ -1,13 +1,34 @@
 package cc.ankin.teambiller.server;
 
+import cc.ankin.teambiller.server.entity.User;
+import cc.ankin.teambiller.server.service.BillService;
+import cc.ankin.teambiller.server.service.UserService;
+import lombok.ToString;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class ServerApplicationTests {
+    @Resource
+    private BillService billService;
+    @Resource
+    private UserService userService;
+
 
     @Test
     void contextLoads() {
+    }
+
+    @Resource
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    void test() {
+        mongoTemplate.save(new User());
     }
 
 }

c287b3c577da1e9e58343e82bee6130070967c3e
yiqunzhang <u7102332@anu.edu.au>
2022-09-20 22:38:16 +1000

    add GlobalExceptionHandler

---
+++ server/src/main/java/cc/ankin/teambiller/server/utils/GlobalExceptionHandler.java
@@ -0,0 +1,72 @@
+package cc.ankin.teambiller.server.utils;
+
+import cc.ankin.teambiller.server.dto.ResponseDto;
+import cc.ankin.teambiller.server.service.ResponseService;
+import cc.ankin.teambiller.server.utils.exception.FailureLoginException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.annotation.Resource;
+
+@RestControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+    @Resource
+    ResponseService responseService;
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseDto handleException(Exception e) {
+        e.printStackTrace();
+        return responseService.newResponseServerException(e);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseDto handleRuntimeException(RuntimeException e) {
+        e.printStackTrace();
+        return responseService.newResponseServerException(e);
+    }
+
+    @ExceptionHandler(FailureLoginException.class)
+    public ResponseDto failureLoginException(RuntimeException e) {
+        e.printStackTrace();
+        return responseService.failureLogin(e);
+    }
+
+//    @ResponseBody
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseDto handleResourceNotFoundException(ResourceNotFoundException e) throws Exception {
+//        e.printStackTrace();
+//        return responseService.newResponseClientException(e);
+//    }
+
+//    @ResponseBody
+//    @ExceptionHandler(FieldInvalidException.class)
+//    public ResponseDto handleFieldInvalidException(FieldInvalidException e) throws Exception {
+//        e.printStackTrace();
+//        return responseService.newResponseApplyInvalid(e);
+//    }
+
+//    @ResponseBody
+//    @ExceptionHandler(ServerException.class)
+//    public ResponseDto handleServerException(ServerException e) throws Exception {
+//        e.printStackTrace();
+//        return responseService.newResponseServerException(e);
+//    }
+
+//    @ResponseBody
+//    @ExceptionHandler(SubmissionException.class)
+//    public ResponseDto handleSubmissionException(SubmissionException e) throws Exception {
+//        e.printStackTrace();
+//        return responseService.newResponseClientException(e);
+//    }
+
+//    @ResponseBody
+//    @ExceptionHandler(LogicException.class)
+//    public ResponseDto handleLogicException(LogicException e) throws Exception {
+//        return responseService.newLogicException(e);
+//    }
+
+
+}
---
+++ server/src/main/java/cc/ankin/teambiller/server/utils/exception/NeedLoginException.java
@@ -0,0 +1,4 @@
+package cc.ankin.teambiller.server.utils.exception;
+
+public class NeedLoginException extends Exception{
+}

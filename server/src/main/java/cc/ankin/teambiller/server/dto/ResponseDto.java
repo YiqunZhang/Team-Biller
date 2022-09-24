e20e83552739daa721ede1f52e5683017f59bd36
yiqunzhang <2223729465@qq.com>
2022-09-25 00:13:55 +1000

    add web response

---
+++ server/src/main/java/cc/ankin/teambiller/server/dto/ResponseDto.java
@@ -0,0 +1,8 @@
+package cc.ankin.teambiller.server.dto;
+
+
+public class ResponseDto {
+    public Integer code;
+    public Object obj;
+    public String message;
+}
---
+++ server/src/main/java/cc/ankin/teambiller/server/service/ResponseService.java
@@ -0,0 +1,76 @@
+package cc.ankin.teambiller.server.service;
+
+import cc.ankin.teambiller.server.dto.ResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResponseService {
+
+    // 0 Ok
+    public ResponseDto ok(Object obj) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 0;
+        responseDto.obj = obj;
+        responseDto.message = "成功";
+
+        return responseDto;
+    }
+
+    // 1 NeedLogin
+    public ResponseDto needLogin(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 1;
+        responseDto.message = "需要登录";
+
+        return responseDto;
+    }
+
+    // 2 NoPermission
+    public ResponseDto noPermission(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 2;
+        responseDto.message = "没有权限";
+        return responseDto;
+    }
+
+    // 3 FailureLogin
+    public ResponseDto failureLogin(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 3;
+        responseDto.message = "登录失败";
+        return responseDto;
+    }
+
+    // 4 Apply Invalid
+    public ResponseDto applyInvalid(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 4;
+        responseDto.message = "请求不合法";
+        return responseDto;
+    }
+
+    // 5 Logic Exception
+    public ResponseDto logicException(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 5;
+        responseDto.message = "逻辑错误";
+        return responseDto;
+    }
+
+    // 8 Client Exception
+    public ResponseDto newResponseClientException(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 8;
+        responseDto.message = "客户端错误";
+        return responseDto;
+    }
+
+    // 9 Exception
+    public ResponseDto newResponseServerException(Exception e) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.code = 9;
+        responseDto.message = "服务器错误";
+        return responseDto;
+    }
+
+}

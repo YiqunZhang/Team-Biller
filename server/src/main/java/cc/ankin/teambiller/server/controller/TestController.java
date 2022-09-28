d81691bb6a6386cb2f19c5eb28b37957c57d09b5
yiqunzhang <2223729465@qq.com>
2022-09-29 01:44:15 +1000

    add test

---
+++ server/src/main/java/cc/ankin/teambiller/server/controller/TestController.java
@@ -0,0 +1,21 @@
+package cc.ankin.teambiller.server.controller;
+
+import lombok.ToString;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController("test")
+@RequestMapping("/api")
+public class TestController {
+
+    @RequestMapping("/test")
+    public String getList() {
+        return "123";
+    }
+
+
+
+}

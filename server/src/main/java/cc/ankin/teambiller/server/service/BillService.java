e1c7e1d8c2bef321744b2afb1b21ae12dce8971a
yiqunzhang <2223729465@qq.com>
2022-10-04 06:37:17 +1100

    add BillService create

--- server/src/main/java/cc/ankin/teambiller/server/service/BillService.java
+++ server/src/main/java/cc/ankin/teambiller/server/service/BillService.java
@@ -1,14 +1,26 @@
 package cc.ankin.teambiller.server.service;
 
 import cc.ankin.teambiller.server.entity.Bill;
+import cc.ankin.teambiller.server.utils.UuidUtils;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
 public class BillService {
+    @Resource
+    private MongoTemplate mongoTemplate;
 
     public Bill add(Bill bill) {
+        return bill;
+    }
+
+    public Bill create(Bill bill) {
+        bill.id = UuidUtils.getUuid();
+        bill.create_time = System.currentTimeMillis();
+        bill.status = 1;
+        mongoTemplate.save(bill);
         return bill;
     }
 

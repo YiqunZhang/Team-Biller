dedd43d967dbc85d33f96083deb69a63d28b454b
yiqunzhang <2223729465@qq.com>
2022-10-05 07:46:56 +1100

    fix some about exception

--- server/src/main/java/cc/ankin/teambiller/server/controller/UserController.java
+++ server/src/main/java/cc/ankin/teambiller/server/controller/UserController.java
@@ -2,12 +2,9 @@ package cc.ankin.teambiller.server.controller;
 
 
 import cc.ankin.teambiller.server.dto.ResponseDto;
-import cc.ankin.teambiller.server.service.ResponseService;
-import cc.ankin.teambiller.server.utils.exception.FailureLoginException;
+import cc.ankin.teambiller.server.utils.ResponseUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 @RestController("user")
 @RequestMapping("/api/user")
@@ -16,7 +13,7 @@ public class UserController {
     @RequestMapping("/login")
     public ResponseDto login() throws Exception {
         Object obj = "123";
-        return ResponseService.ok(obj);
+        return ResponseUtils.ok(obj);
     }
 
 }

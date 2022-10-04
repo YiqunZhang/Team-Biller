fff2232d28e1f0cacc12a721f8217406c2114015
yiqunzhang <2223729465@qq.com>
2022-10-05 09:27:30 +1100

    fix about payment status

--- server/src/main/java/cc/ankin/teambiller/server/entity/Payment.java
+++ server/src/main/java/cc/ankin/teambiller/server/entity/Payment.java
@@ -7,10 +7,14 @@ public class Payment {
     public List<String> billIdList;
     public String payerId;
     public String method; // 转账方式, 例如: 支付宝, 微信
-    public Integer status; // 0 已删除; 1 已被付款人撤销; 2 待支付; 3 收款人已拒绝; 4 已支付待确认; 5 收款人确认已完成;
+
+    // 从未创建 -> 已支付待确认 -> 收款人手动确认已完成 || 收款人自动确认已完成 || 收款人已拒绝 || 已被付款人撤销
+    public Integer status; // 0 已删除; 1 已被付款人撤销; 2 待支付; 3 收款人已拒绝; 4 已支付待确认; 5 收款人手动确认已完成; 6 收款人自动确认已完成;
+    public String remark; // 备注, 付款人填写
+    public String reason; // 用于拒绝或撤销
     public Integer amount; // 100 times
     public List<String> imageList;
-    public String remark;
+
     public Long create_time;
     public Long pay_time;
     public Long confirm_time;

e5f95b21a9517bf48327c0f89edef9035ca2af52
yiqunzhang <2223729465@qq.com>
2022-10-05 09:13:54 +1100

    add Payment

---
+++ server/src/main/java/cc/ankin/teambiller/server/entity/Payment.java
@@ -0,0 +1,19 @@
+package cc.ankin.teambiller.server.entity;
+
+import java.util.List;
+
+public class Payment {
+    public String id;
+    public List<String> billIdList;
+    public String payerId;
+    public String method; // 转账方式, 例如: 支付宝, 微信
+    public Integer status; // 0 已删除; 1 已被付款人撤销; 2 待支付; 3 收款人已拒绝; 4 已支付待确认; 5 收款人确认已完成;
+    public Integer amount; // 100 times
+    public List<String> imageList;
+    public String remark;
+    public Long create_time;
+    public Long pay_time;
+    public Long confirm_time;
+    public Long decline_time;
+
+}
